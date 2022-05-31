9cff107b23ba6f3fa1ade5dcf24024ab8fb58635
pedrokpdo <pedromotoehm@gmail.com>
2022-05-31 10:12:23 -0300

    Two Button Container

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Describe } from '../../components/Describe'
 import { NavBar } from '../../components/NavBar'
 import { SimpleButton } from '../../components/SimpleButton'
+import { TwoButtonContainer } from '../../components/TwoButtonContainer'
 import { Container } from './styles'
 
 
@@ -9,8 +10,11 @@ export const Home = () => {
     return (
         <Container>
             <NavBar />
-            <Describe title='Olá Visitante' titleText='Aqui você pode marcar um horario ou pode solicitar um corte de emergencia, voce só precisa de um cadastro.'/>
-            <SimpleButton buttonText='Login'/>
+            <Describe title='Olá Visitante' titleText='Aqui você pode marcar um horario ou pode solicitar um corte de emergencia, voce só precisa de um cadastro.' />
+            <TwoButtonContainer>
+                <SimpleButton buttonText='Login' />
+                <SimpleButton buttonText='Cadastrar' />
+            </TwoButtonContainer>
         </Container>
     )
 }
